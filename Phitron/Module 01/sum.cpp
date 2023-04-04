bdf5b76a3978b3e1428045853216b115fb10d2a0
MOIN <129009554+MOIN46@users.noreply.github.com>
2023-04-04 16:10:48 +0600

    Add files via upload

---
+++ Phitron/Module 01/sum.cpp
@@ -0,0 +1,30 @@
+/**
+ *    author:  MOIN46
+ *          --MD:MOIN UDDIN BHUYAIN TUHIN
+ *          --CSE,BRUR
+ *          --Email:cse118199brur@gmail.com
+ **/
+
+#include <bits/stdc++.h>
+#define endl "\n"
+#define EPS 1E-9
+#define PI acos(-1)
+#define ll long long int
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define p(condition) cout << (condition ? "YES" : "NO")
+#define me ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main()
+{
+    me;
+   int a,b,sum=0;
+   cin>>a>>b;
+   //while(a--)
+  sum+=a+b;
+   cout<<sum<<"\n";
+
+#ifndef ONLINE_JUDGE
+    cout << "\nExecution Time : " << 1.0 * clock() / CLOCKS_PER_SEC << "s ";
+#endif
+}
